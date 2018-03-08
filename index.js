d15860b7331fea19eea875db58bd50b344d594d7
Raphael Kieling <rapha_kieling@hotmail.com>
2018-03-08 13:39:04 -0300

    Heroku base, sequelize

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const app = require('./src');
-const PORT = process.env.PORT || 3000;
+const app = require("./src");
+const models = require("./src/models");
 
-app.listen(PORT,()=>{
+function listenServer() {
+  const PORT = process.env.PORT || 3000;
+
+  app.listen(PORT, () => {
     console.log(`Listening in port ${PORT}`);
-})
+  });
+}
+
+models.sequelize.sync({ force:true }).then(() => {
+  listenServer();
+});
