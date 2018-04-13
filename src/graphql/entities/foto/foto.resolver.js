ba19c7632695d4b5d92c0abf8287f93ddc200383
Raphael Kieling <rapha_kieling@hotmail.com>
2018-04-13 14:50:43 -0300

    imagem ajustado com png jpg gif

--- src/graphql/entities/foto/foto.resolver.js
+++ src/graphql/entities/foto/foto.resolver.js
@@ -11,13 +11,18 @@ const fotoResolver = {
             }
 
             let base = '';
-            let buffer = Buffer.from(foto.get('base64').replace(/^data:image\/jpeg;base64,/, ''), 'base64');
+            let buffer = Buffer.from(foto.get('base64').replace(/^data:image\/(jpeg|png|jpg|gif);base64,/, ''), 'base64');
 
-            await Jimp.read(buffer).then((marker) => {
-                marker.resize(50, 50).getBuffer(Jimp.MIME_JPEG, (err, newMarkerBuffer) => {
-                    base = `data:image/jpeg;base64,${newMarkerBuffer.toString('base64')}`;
+            await Jimp.read(buffer)
+                .then((marker) => {
+                    marker.resize(50, 50).getBuffer(Jimp.MIME_JPEG, (err, newMarkerBuffer) => {
+                        if (err) return;
+
+                        base = `data:image/jpeg;base64,${newMarkerBuffer.toString('base64')}`;
+                    })
+                }).catch((err) => {
+                    console.log('Erro')
                 })
-            })
 
             return base;
         }
